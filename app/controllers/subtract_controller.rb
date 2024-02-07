d221db752db5480b1341eacc01ad52aa0a202f15
Dylan Menard <148174649+Dylanm22@users.noreply.github.com>
2024-02-07 19:20:23 +0000

    omnicalcdebug

---
+++ app/controllers/subtract_controller.rb
@@ -0,0 +1,17 @@
+class SubtractController < ApplicationController
+  def show_sub_form
+    render({ :template => "subtraction_templates/subtraction_form" })
+  end
+
+  def subtract
+    @first_number = params.fetch("FirstNum").to_f
+    @second_number = params.fetch("second_num").to_f
+
+    @result =  @second_number - @first_number
+
+    render({ :template => "subtraction_templates/sub_results" })
+  end
+
+
+
+end
